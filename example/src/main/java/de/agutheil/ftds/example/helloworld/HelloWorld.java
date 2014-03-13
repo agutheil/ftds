2e62e4ff52811617316c75dbbc8de196207d009f
agutheil <andreas.gutheil@gmail.com>
2014-03-13 12:34:04 +0100

    Step2: Nachricht als Argument

--- example/src/main/java/de/agutheil/ftds/example/helloworld/HelloWorld.java
+++ example/src/main/java/de/agutheil/ftds/example/helloworld/HelloWorld.java
@@ -3,7 +3,12 @@ package de.agutheil.ftds.example.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		if(args.length < 1){
+			System.err.println("Keine Nachricht uebergeben!");
+			System.exit(1);
+		}
+		String message = args[0];
+		System.out.println(message);
 
 	}
 
